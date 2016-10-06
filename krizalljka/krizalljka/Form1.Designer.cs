20ae67062467acee21f4f5bf8d70822ca26064b7
ana098 <ana098@gmail.com>
2016-10-06 21:09:52 +0200

    Add files via upload

---
+++ krizalljka/krizalljka/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace krizalljka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openNewGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.board = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.provjeriTočnostToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.board)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(619, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openNewGameToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.provjeriTočnostToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openNewGameToolStripMenuItem
+            // 
+            this.openNewGameToolStripMenuItem.Name = "openNewGameToolStripMenuItem";
+            this.openNewGameToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.openNewGameToolStripMenuItem.Text = "Open new game";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // board
+            // 
+            this.board.AllowUserToAddRows = false;
+            this.board.AllowUserToDeleteRows = false;
+            this.board.AllowUserToResizeColumns = false;
+            this.board.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.MenuText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.board.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.board.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.board.ColumnHeadersVisible = false;
+            this.board.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column15,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(1);
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.board.DefaultCellStyle = dataGridViewCellStyle2;
+            this.board.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.board.Location = new System.Drawing.Point(0, 24);
+            this.board.Name = "board";
+            this.board.RowHeadersVisible = false;
+            this.board.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.board.Size = new System.Drawing.Size(619, 507);
+            this.board.TabIndex = 1;
+            this.board.Tag = "";
+            this.board.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.board_CellContentClick);
+            this.board.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.board_CellValueChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "Column12";
+            this.Column12.Name = "Column12";
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Column13";
+            this.Column13.Name = "Column13";
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "Column14";
+            this.Column14.Name = "Column14";
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "Column15";
+            this.Column15.Name = "Column15";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Column10";
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Column11";
+            this.Column11.Name = "Column11";
+            this.Column11.ReadOnly = true;
+            // 
+            // provjeriTočnostToolStripMenuItem
+            // 
+            this.provjeriTočnostToolStripMenuItem.Name = "provjeriTočnostToolStripMenuItem";
+            this.provjeriTočnostToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.provjeriTočnostToolStripMenuItem.Text = "Provjeri točnost";
+            this.provjeriTočnostToolStripMenuItem.Click += new System.EventHandler(this.provjeriTočnostToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 531);
+            this.Controls.Add(this.board);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.board)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openNewGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.DataGridView board;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.ToolStripMenuItem provjeriTočnostToolStripMenuItem;
+    }
+}
+
---
+++ krizalljka/krizalljka/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace krizalljka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inicijalizacija();
+            
+            for (int r = 0; r < 5; r++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    slika(r, c);
+                }
+            }
+
+            for (int r = 7; r < 10; r++)
+            {
+                for (int c = 5; c < 10; c++)
+                {
+                    slika(r, c);
+                }
+            }
+
+            PostaviPitanje(1,4,"Pravomoćna sudska odluka");
+            PostaviPitanje(2, 4, "Telekomu- nikacijski uređaj");
+            PostaviPitanje(3, 4, "Pohvalne pjesme");
+            PostaviPitanje(4, 4, "Majka odmila");
+            PostaviPitanje(5, 4, "Stanovnica Irana --------  Ocrt");
+            PostaviPitanje(5, 3, "Dronjci, krpe");
+            PostaviPitanje(5, 2, "Sin Ivice Osima");
+            PostaviPitanje(5, 1, "Nogometaš Fabregas");
+            PostaviPitanje(6, 0, "X (panamska misica)");
+            PostaviPitanje(7, 0, "Redatelj Kusturica");
+            PostaviPitanje(8, 0, "Šezdeset minuta (mn.)");
+            PostaviPitanje(9, 0, "Naš otok bjeloglavih supova");
+            PostaviPitanje(0, 5, "Mjera alkohol- iziranosti vozača");
+            PostaviPitanje(0, 6, "Čuvari reda");
+            PostaviPitanje(0, 7, "Pra- stanovnik Njemačke");
+            PostaviPitanje(0, 8, "Venecija");
+            PostaviPitanje(0,9,  "Redateljica Kodar");
+            PostaviPitanje(0, 10, "Repriza");
+            PostaviPitanje(0, 11, "Jedanako, identično");
+            PostaviPitanje(0, 12, "Jennifer Aniston");
+            PostaviPitanje(0, 13, "Austrijski pisac Canneti");
+            PostaviPitanje(0, 14, "Činiti kadrove filma");
+            PostaviPitanje(3, 8, "Uredski spis -----  Tenisačica Konjuh");
+            PostaviPitanje(4, 9, "Vodonoša -------- Kelly Brook");
+            PostaviPitanje(2, 10, "Dvorana ----- Mehanički defekt");
+            PostaviPitanje(3, 12, "Ivan Dodig ------ Doticaji");
+            PostaviPitanje(5, 11, "Apscise i ordinate ----- Pronalazak");
+            PostaviPitanje(6, 13, "Radijus ------ Mamljenje, vabljenje");
+            PostaviPitanje(7, 10, "Najhladnije godišnje doba");
+            PostaviPitanje(8, 10, "Sol mokraćne kiseline");
+            PostaviPitanje(9, 10, "Glumica Rogers");
+
+
+            Odgovori(1, 5, "P");
+            Odgovori(1, 6, "R");
+            Odgovori(1, 7, "A");
+            Odgovori(1, 8, "V");
+            Odgovori(1, 9, "O");
+            Odgovori(1, 10, "R");
+            Odgovori(1, 11, "I");
+            Odgovori(1, 12, "J");
+            Odgovori(1, 13, "E");
+            Odgovori(1, 14, "K");
+            Odgovori(2, 5, "R");
+            Odgovori(2, 6, "E");
+            Odgovori(2, 7, "L");
+            Odgovori(2, 8, "E");
+            Odgovori(2, 9, "J");
+            Odgovori(2, 11, "S");
+            Odgovori(2, 12, "A");
+            Odgovori(2, 13, "L");
+            Odgovori(2, 14, "A");
+            Odgovori(3, 5, "O");
+            Odgovori(3, 6, "D");
+            Odgovori(3, 7, "E");
+            Odgovori(3, 9, "A");
+            Odgovori(3, 10, "K");
+            Odgovori(3, 11, "T");
+            Odgovori(3, 13, "I");
+            Odgovori(3, 14, "D");
+            Odgovori(4, 5, "M");
+            Odgovori(4, 6, "A");
+            Odgovori(4, 7, "M");
+            Odgovori(4, 8, "A");
+            Odgovori(4, 10, "V");
+            Odgovori(4, 11, "O");
+            Odgovori(4, 12, "D");
+            Odgovori(4, 13, "A");
+            Odgovori(4, 14, "R");
+            Odgovori(5, 5, "I");
+            Odgovori(5, 6, "R");
+            Odgovori(5, 7, "A");
+            Odgovori(5, 8, "N");
+            Odgovori(5, 9, "K");
+            Odgovori(5, 10, "A");
+            Odgovori(5, 12, "O");
+            Odgovori(5, 13, "S");
+            Odgovori(5, 14, "I");
+            Odgovori(6, 1, "C");
+            Odgovori(6, 2, "A");
+            Odgovori(6, 3, "R");
+            Odgovori(6, 4, "O");
+            Odgovori(6, 5, "L");
+            Odgovori(6, 6, "I");
+            Odgovori(6, 7, "N");
+            Odgovori(6, 8, "E");
+            Odgovori(6, 9, "B");
+            Odgovori(6, 10, "R");
+            Odgovori(6, 11, "I");
+            Odgovori(6, 12, "D");
+            Odgovori(6, 14, "R");
+            Odgovori(7, 1, "E");
+            Odgovori(7, 2, "M");
+            Odgovori(7, 3, "I");
+            Odgovori(7, 4, "R");
+            Odgovori(7, 11, "Z");
+            Odgovori(7, 12, "I");
+            Odgovori(7, 13, "M");
+            Odgovori(7, 14, "A");
+            Odgovori(8, 1, "S");
+            Odgovori(8, 2, "A");
+            Odgovori(8, 3, "T");
+            Odgovori(8, 4, "I");
+            Odgovori(8, 11, "U");
+            Odgovori(8, 12, "R");
+            Odgovori(8, 13, "A");
+            Odgovori(8, 14, "T");
+            Odgovori(9, 1, "C");
+            Odgovori(9, 2, "R");
+            Odgovori(9, 3, "E");
+            Odgovori(9, 4, "S");
+            Odgovori(9, 11, "M");
+            Odgovori(9, 12, "I");
+            Odgovori(9, 13, "M");
+            Odgovori(9, 14, "I");
+
+        }
+        private void PostaviPitanje(int red, int kolona, string pitanje)
+        {
+
+            DataGridViewCell c = board[kolona, red];
+            c.Style.BackColor = Color.Gray;
+            c.Style.ForeColor = Color.Black;
+            c.ReadOnly = true;
+            c.Style.SelectionBackColor = Color.Black;
+            c.Style.SelectionForeColor = Color.White;
+            c.Style.WrapMode = DataGridViewTriState.True;
+            c.Value = pitanje;
+            c.Style.Font = new Font("Arial", 6);
+            
+        }
+        private void Odgovori(int red, int kolona, string slovo)
+        {
+            DataGridViewCell c = board[kolona, red];
+            
+            c.ReadOnly = false;
+            c.Tag = slovo;
+            c.Style.Font = new Font("Arial", 16);
+            c.Style.SelectionBackColor = Color.LightYellow;
+            c.Style.SelectionForeColor = Color.Black;
+            
+            
+        }
+        private void inicijalizacija()
+        {
+            
+                board.Rows.Add(10);
+                foreach (DataGridViewColumn a in board.Columns)
+                    a.Width = board.Width / board.Columns.Count;
+                foreach (DataGridViewRow b in board.Rows)
+                    b.Height = board.Height / board.Rows.Count;
+
+                for (int red = 0; red < board.Rows.Count; red++)
+                {
+                    for (int kolona = 0; kolona < board.Columns.Count; kolona++)
+                    {
+                        board[kolona, red].ReadOnly = true;
+                        
+                    }
+                }
+               
+        
+        }
+
+        private void formatCell(int red, int kolona, string slovo)
+        {
+            
+            DataGridViewCell c = board[kolona, red];
+            
+            c.ReadOnly = false;
+            c.Tag = slovo;
+        }
+
+        private void slika(int red, int kolona)
+        { 
+            DataGridViewCell c= board[kolona, red];
+            c.ReadOnly = true;
+            c.Style.BackColor = Color.Black;
+        }
+
+        private void provjeriTočnostToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            board.EndEdit();
+
+            for (int r = 0; r < board.Rows.Count -1; r++)
+            {
+                for (int k = 0; k < board.Columns.Count -1; k++)
+                {
+                    DataGridViewCell c = board[k, r];
+
+                    if (!c.ReadOnly && c.Value!=null)
+                    {
+                        if (c.Value.ToString().ToUpper() != c.Tag.ToString().ToUpper())
+                        {
+                            c.Style.BackColor = Color.Red;
+                        }
+                        else
+                        {
+                            c.Style.BackColor = Color.Green;
+                        }
+                    } 
+                }
+            }
+        }
+
+        private void board_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+     
+
+        private void board_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+            DataGridViewCell c = board[e.ColumnIndex, e.RowIndex];
+            if (!c.ReadOnly && c.Value != null)
+            {
+                if (c.Value.ToString().Trim().Length > 0)
+                    c.Value = c.Value.ToString().Substring(0, 1).ToUpper();
+            }
+        }
+    }
+
+}
